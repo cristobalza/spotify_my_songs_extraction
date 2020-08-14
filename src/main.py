a7a852fe492c1798d25a76d172ed6ff1c6bb857e
Cristobal Zamorano Astudillo <cristobal.zamorano@berkeley.edu>
2020-08-13 18:20:37 -0700

    working of getting the stream history count

--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
 
-import streaming_music
+import my_songs_tool
 from your_data import *
 import pandas as pd
 from time import sleep
 from your_data import *
 
 def run():
+    """
+    Run this function and all the will extract all the music from the JSON files and add
+    the Spotify's features.
+    """
 
-    # Obtain access through the API
-    token = streaming_music.get_token(username, client_id, client_secret, redirect_uri, scope)
-    print(token)
+    # Step 1: Get Authorization
+    token = my_songs_tool.get_authorization(username, client_id, client_secret, redirect_uri, scope)
+
+    # Step 2: Get streaming list of songs
+    path = 'data/'
+    streamings = my_songs_tool.get_streamings(path)
+    print(f'You have {len(streamings)} streamings. Good job at listing Spotify')
+
     
 if __name__ == '__main__':
     run()
